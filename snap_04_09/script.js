89b72eb4ae8eceee26d3d52eb4d6379e4a4b7ad5
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-04-09 14:18:37 -0700

    For the Snap assignment I learned the basics of using functions in order to multiply two values together and assigning variables for each. Then I used a similar function for adding to strings together to created a first and last name.

--- snap_04_09/script.js
+++ snap_04_09/script.js
@@ -1,11 +1,41 @@
 
 
-function result(value1, value2) {
+/*function result(value1, value2) {
 	let value1 = 25;
 	let value2 = 4;
 	let varMultiple = value1 * value2;
 	console.log(varMultiple)
 }
 
-result(11, 5);
+result(11, 5);*/
 
+/**
+ * Take two numbers and multiply them.
+ *
+ * @param int firstNumber One number to multiply.
+ * @param int secondNumber The other number to multiply.
+ *@return int The product of the two numbers.
+ */
+function multiplyNumbers (firstNumber, secondNumber) {
+	return firstNumber * secondNumber;
+}
+
+let myNumber = -5;
+
+console.log(multiplyNumbers(5, 10));
+console.log(multiplyNumbers(-2, myNumber));
+
+/**
+ *Take two strings(Names) and concatenate them into a full name.
+ *A string are a collection of any characters in single of double quotes.
+ *
+ *@param string firstName The given name to concatenate
+ * @param string lastName The family name to concatenate.
+ * @return string The full name of our person.
+ */
+
+function getFullName(firstName, lastName) {
+	return firstName + ' ' + lastName;
+}
+
+console.log(getFullName("Paul", "Schulzetenberg"));
