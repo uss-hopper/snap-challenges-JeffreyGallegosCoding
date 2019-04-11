1c1aa67fb6b84df498c6f81f815875c5fc6459bb
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-04-11 09:19:49 -0700

    For this snap assignment 2 I wrote a user story and attempted to create an ERD for the Mailchimp site for uploading a picture.

---
+++ snap-04-11/erd.php
@@ -0,0 +1,16 @@
+<DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>ERD-Upload Picture</title>
+		</head>
+		<body>
+			<h1>ERD</h1>
+			<div>
+				<img src="ERD-Snap-Challenge.PNG" alt="ERD">
+			</div>
+			<div>
+				<a href="userprofile.php">User Profile</a>
+			</div>
+		</body>
+	</html>
---
+++ snap-04-11/userprofile.php
@@ -0,0 +1,15 @@
+<DOCTYPE html>
+	<html lang="en">
+		<head>
+			<meta charset="UTF-8">
+			<title>Snap Challenge User Profile</title>
+		</head>
+		<body>
+			<h1>User Story</h1>
+				<p>As a power user I would like to use Mailchimp to insert pictures along with my written messages that I send
+					out to clients and subscribers.</p>
+			<div>
+				<a href="erd.php">ERD</a>
+			</div>
+		</body>
+	</html>
