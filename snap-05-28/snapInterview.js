1902f0203ce2e224581c92ab864c7a2df4e4046f
Jeffrey Gallegos <jeffreygallegos01@gmail.com>
2019-05-28 08:50:23 -0700

    Attempt at question 2.

--- snap-05-28/snapInterview.js
+++ snap-05-28/snapInterview.js
@@ -1,5 +1,24 @@
+
+//Question 1
 array1 = [1, 2, 2, 4, 5, 5, 7];
 
 for (i=0, i<=7, i++1)
 	int count;
+
+
+
+//Question 2
+array2 = [5, 26];
+
+i=0;
+while(i <= 26) {
+	if(i/2 === even)
+		output fizz;
+
+	else if(i/3 === )
+		output buzz;
+
+	else if(i === fizz and buzz)
+		output baz;
+}
 
